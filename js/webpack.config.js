745edf8be02f3a12949774646afa0339f08d3540
Caio Ferrarezi <caioferrarezi2@gmail.com>
2021-02-21 13:18:06 -0300

    :construction: wip(default) add first game demo

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,42 +3,49 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  output: {
-    filename: '[name]-[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    modules: ['node_modules'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
+const OUTPUT_PATH = {
+  'development': 'dist',
+  'production': '../demo'
+}
+
+module.exports = (env) => {
+  return {
+    mode: env.NODE_ENV,
+    entry: './src/index.ts',
+    output: {
+      filename: '[name]-[hash].js',
+      path: path.resolve(__dirname, OUTPUT_PATH[env.NODE_ENV])
     },
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }, {
-        test: /\.(wav)$/i,
-        loader: 'file-loader'
-      }, {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }, {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
+    resolve: {
+      modules: ['node_modules'],
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+      },
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.(wav)$/i,
+          loader: 'file-loader'
+        }, {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }, {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+  }
 }
